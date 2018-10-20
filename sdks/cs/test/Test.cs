ee04b6cc5245bd07fc71a76bba728f93d8d7f872
= <murnkamp@gmail.com>
2018-10-20 10:18:46 -0400

    Added GetPossibleMoves

--- sdks/cs/test/Test.cs
+++ sdks/cs/test/Test.cs
@@ -1,4 +1,6 @@
 ﻿using ai;
+using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using Xunit;
 using FluentAssertions;
@@ -21,6 +23,20 @@ namespace test
             obj.board[3][3].Should().NotBe(0);
 
         }
-        
+
+        [Fact]
+        public void FindMoves()
+        {
+            const string input = @"{""board"":[[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,1,0,0,0],[0,0,0,2,1,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]],""maxTurnTime"":15000,""player"":2}";
+            var obj = JsonConvert.DeserializeObject<GameMessage>(input);
+
+            List<int[]> moves = AI.GetPossibleMoves(obj.board, obj.player);
+
+            foreach (int[] move in moves)
+            {
+                Console.WriteLine("Move: " + move[0] + ", " + move[1]);
+            }
+        }
+
     }
 }
